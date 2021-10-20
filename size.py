250396df4ce3681a2fad4cb1ad229c5c0deda2ca
Shaan10-01 <56250197+Shaan10-01@users.noreply.github.com>
2021-10-20 23:07:18 +0530

    Add files via upload

---
+++ size.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 21:01:24 2020
+
+@author: Shaan
+"""
+
+import os
+file_size = os.path.getsize("selection sort.py")
+print("\nThe size of selection sort.py is :",file_size,"Bytes")
+print()
